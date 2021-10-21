ac0f6c3f07f9879eeaeed325a6f57450d861bd49
rashi <rashid058@mial.ru>
2021-10-21 18:22:11 +0500

    1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,20 @@
 package jm.task.core.jdbc.util;
+import com.mysql.cj.xdevapi.SessionFactory;
+import jm.task.core.jdbc.service.model.User;
+
+import javax.imageio.spi.ServiceRegistry;
+import javax.security.auth.login.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class Util {
     private static Util instance = null;
     private static Connection connection = null;
+    private static SessionFactory sessionFactory = null;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/userdb?useSSL=false&serverTimezone=Asia/Shanghai";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "123456";
@@ -19,6 +27,28 @@ public class Util {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", USERNAME)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void closeConnection() {
+        if (sessionFactory != null)
+            sessionFactory.close();
+    }
     }
 
     public static Util getInstance() {
@@ -30,4 +60,6 @@ public class Util {
     public Connection getConnection(){
         return connection;
     }
+
+
 }
